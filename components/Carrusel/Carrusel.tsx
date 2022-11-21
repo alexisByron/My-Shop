fde0b8c4c1513b58fdef4bfccaafbebfb69c49de
alexis moya <alexis.moya.14@gmail.com>
2022-11-20 22:20:05 -0300

    preparacion antes de empezar trabajo por ramas
    ya tengo una base mas o menos decente para mejorar, agregando mejor tipado de ts  y logica de componentes ademas de diseño, pensando si usar un preprosesador de estilos como material

--- components/Carrusel/Carrusel.tsx
+++ components/Carrusel/Carrusel.tsx
@@ -4,7 +4,7 @@ import { Carousel } from "react-responsive-carousel";
 
 export default function CustomCarrusel() {
   return (
-    <Carousel autoPlay infiniteLoop interval={100000} showThumbs={false}>
+    <Carousel autoPlay infiniteLoop interval={5000} showThumbs={false}>
       <div className={styles.sliderContainerImg}>
         <h1 className={styles.textImage}>Yo que se trabajamos muy bien <br/> Suban el sueldo..</h1>
         <img src='https://www.elmuneco.cl/wp-content/uploads/2019/05/distribuidora-el-mu%C3%B1eco-02.jpg' />
--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,28 +1,29 @@
 export default function ProductList() {
+  
   const productos = [
     {
       id: 1,
-      nombre: "Producto 1",
-      precio: 1000,
-      descripcion: "Descripcion del producto 1",
+      nombre: "ATUN VAN CAMPS EN AGUA 160 GRS.",
+      precio: '$1.235 por unidad',
+      descripcion: "Formato: 1 x 1 unit",
       imagen:
-        "https://www.elmuneco.cl/wp-content/uploads/2019/05/el-muñeco-distribuidora-logo.png",
+        "https://res.cloudinary.com/yom/image/upload/h_200/v1587054190/blloozitpex6lbfkq9wr.jpg",
     },
     {
       id: 2,
-      nombre: "Producto 2",
-      precio: 2000,
-      descripcion: "Descripcion del producto 2",
+      nombre: "CROCANTE DE POLLO 100 GRS",
+      precio: '$19.399 por unidad',
+      descripcion: "Formato: 1 x 1 unit",
       imagen:
-        "https://www.elmuneco.cl/wp-content/uploads/2019/05/el-muñeco-distribuidora-logo.png",
+        "https://res.cloudinary.com/yom/image/upload/h_200/v1587052262/iccl9pu3zghbrfxqlcoi.png",
     },
     {
       id: 3,
-      nombre: "Producto 3",
-      precio: 3000,
-      descripcion: "Descripcion del producto 3",
+      nombre: "PINA EN RODAJAS ESMERALDA 567 GRS. - I.C.B.",
+      precio: '$1.214 por unidad',
+      descripcion: "Formato: 1 x 1 unit",
       imagen:
-        "https://www.elmuneco.cl/wp-content/uploads/2019/05/el-muñeco-distribuidora-logo.png",
+        "https://res.cloudinary.com/yom/image/upload/h_200/v1503327115/dwql9cgs4iqhw16vcgjm.jpg",
     },
     {
       id: 4,
@@ -48,23 +49,34 @@ export default function ProductList() {
       imagen:
         "https://www.elmuneco.cl/wp-content/uploads/2019/05/el-muñeco-distribuidora-logo.png",
     },
-    {
-      id: 7,
-      nombre: "Producto 7",
-      precio: 7000,
-      descripcion: "Descripcion del producto 7",
-      imagen:
-        "https://www.elmuneco.cl/wp-content/uploads/2019/05/el-muñeco-distribuidora-logo.png",
-    },
   ];
+
   return (
     <div style={{ display: "grid", gridTemplateColumns: "33.3% 33.3% 33.3%" }}>
       {productos.map((producto) => (
-        <div key={producto.id} style={{padding:10,margin:10,boxShadow: "0px 0px 10px 0px rgba(33, 12, 51, 1)",}}>
-          <h1>{producto.nombre}</h1>
-          <p>{producto.descripcion}</p>
-          <p>{producto.precio}</p>
-          <img src={producto.imagen} alt={producto.nombre} />
+        <div
+          key={producto.id}
+          style={{
+            padding: 10,
+            margin: 10,
+            boxShadow: "0px 0px 10px 0px rgba(33, 12, 51, 1)",
+            backgroundColor: "rgba(219, 177, 255, 1)",
+            borderRadius: 10,
+          }}
+        >
+          <img
+            src={producto.imagen}
+            alt={producto.nombre}
+            style={{ width: "100%", height:130, objectFit:'contain' }}
+          />
+          <h3 style={{ color: "rgba(33, 12, 51, 1)" }}>{producto.nombre}</h3>
+          <p style={{ color: "rgba(33, 12, 51, 1)" }}>{producto.descripcion}</p>
+          <p style={{ color: "rgba(33, 12, 51, 1)" }}>{producto.precio}</p>
+          <div style={{width:'100%', textAlign:'center'}}>
+          <button style={{ backgroundColor: "rgba(33, 12, 51, 1)", height:50, width:200 }}>
+            Agregar a carrito
+          </button>
+          </div>
         </div>
       ))}
     </div>
