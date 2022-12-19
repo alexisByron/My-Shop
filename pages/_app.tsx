e3c46f90d62e928064773ccf775537fd2dbaf0b0
alexis moya <alexis.moya.14@gmail.com>
2022-12-19 00:00:28 -0300

    add sweet modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { persistor, store } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <Provider store={store} >
       <PersistGate loading={null} persistor={persistor}>
         <Component {...pageProps} />
       </PersistGate>
