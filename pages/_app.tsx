8c9227c640831ac3c7f0338941d54fc7a5753fca
alexis moya <alexis.moya.14@gmail.com>
2023-01-07 18:51:03 -0300

    google ads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,23 @@ import { Provider } from "react-redux";
 import { persistor, store } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import React from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store} >
+    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+        <Script
+          id='Adsense-id'
+          data-ad-client='ca-pub-9125227049757827'
+          async
+          strategy='afterInteractive'
+          onError={(e) => {
+            console.error("Script failed to load", e);
+          }}
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+        />
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
